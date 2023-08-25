536c7dc722ac6baf5cf9f249da6f831ac642c901
Dominika <dominika-biedulska@tlen.pl>
2023-08-25 13:01:49 +0200

    feat: add content to Intro

--- src/components/organisms/IntroContent.vue
+++ src/components/organisms/IntroContent.vue
@@ -1,10 +1,27 @@
 <template>
-  <div></div>
+  <div
+    class="w-full bg-test h-[550px] flex items-center justify-center font-open"
+  >
+    <div class="mx-[500px] text-center text-white">
+      <p class="text-[45px] mb-10 font-semibold">
+        Zastanawiasz się, jak sprawdzić swój poziom językowy?
+      </p>
+      <p class="text-xl">
+        Każdy z nas przynajmniej raz w życiu próbował uczyć się języka obcego.
+        Aplikacja Lang4You pomoże Ci sprawdzić, jaki efekt przyniosły te próby.
+        Dzięki specjalnie opracowanym testom, sprawdzisz swój poziom i dowiesz
+        się jakie partie materiału są do powtórki, a w jakich jesteś naprawdę
+        doskonały 👌. Po wykonaniu testu otrzymujesz jego wynik wraz z
+        odpowiedziami i oznaczeniem, w którym miejscu popełniłeś błąd. To jak,
+        startujemy?
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent ({
-  name: 'IntroContent'
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "IntroContent",
 })
 </script>
