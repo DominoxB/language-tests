81836cc15c711ca5a0725287786b80664e0406af
Dominika <dominika-biedulska@tlen.pl>
2023-08-29 09:49:12 +0200

    feat: create name and logo molecule

---
+++ src/components/molecules/NameAndLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center">
+    <img src="../icons/logo.png" alt="logo" class="w-12 mr-2" />
+    <span class="font-courgette text-xl">Lang4You</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent ({
+  name: 'NameAndLogo',
+  components: {
+  }
+})
+</script>
