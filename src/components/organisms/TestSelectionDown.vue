7e9c47b42feb3349a605f36d631ef4da2ab5f48a
Dominika <dominika-biedulska@tlen.pl>
2023-09-06 13:06:26 +0200

    feat: add component to the view

---
+++ src/components/organisms/TestSelectionDown.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-slate-100 py-10 font-open text-center">
+    <span class="text-2xl">Wybierz test i sprawdź swój poziom zaawansowania:</span>
+    <div class="flex justify-center mt-12 space-x-16">
+      <TestSelectionDownItem name="Angielski" flag="https://img.icons8.com/offices/30/great-britain.png" />
+      <TestSelectionDownItem name="Rosyjski" flag="https://img.icons8.com/offices/30/russian-federation.png" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TestSelectionDownItem from '../atoms/TestSelectionDownItem.vue'
+export default defineComponent({
+  name: 'TestSelectionDown',
+  components: {
+    TestSelectionDownItem
+  },
+  // setup() {
+  //   const english = {
+  //     name: 'English',
+  //     flag: 'https://img.icons8.com/offices/30/great-britain.png'
+  //   }
+  //   const russian = {
+  //     name: 'Russian',
+  //     flag: 'https://img.icons8.com/offices/30/russian-federation.png'
+  //   }
+  //   return {
+  //     english,
+  //     russian
+  //   }
+  // }
+})
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,7 @@
     <TopNavbar />
     <IntroContent />
     <AboutUs />
+    <TestSelectionDown />
   </div>
 </template>
 
@@ -11,12 +12,14 @@ import { defineComponent } from 'vue'
 import IntroContent from '../components/organisms/IntroContent.vue'
 import TopNavbar from '../components/organisms/TopNavbar.vue'
 import AboutUs from '@/components/organisms/AboutUs.vue'
+import TestSelectionDown from '@/components/organisms/TestSelectionDown.vue'
 export default defineComponent ({
   name: 'HomePage',
   components: {
     IntroContent,
     TopNavbar,
-    AboutUs
+    AboutUs,
+    TestSelectionDown
   }
 })
 </script>
