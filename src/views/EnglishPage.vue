bb247ba59da69dfdb947ce74e15aa7d384f35bfb
Dominika <dominika-biedulska@tlen.pl>
2023-09-07 11:40:28 +0200

    feat: add component and bg

---
+++ src/views/EnglishPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-english h-[550px] w-full">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent ({
+  name: 'EnglishPage',
+})
+
+</script>
