3e9cfd26fa4782c7c50fdc6479a6488e08a154ac
Dominika <dominika-biedulska@tlen.pl>
2023-09-07 12:38:58 +0200

    feat: add component

--- src/views/EnglishPage.vue
+++ src/views/EnglishPage.vue
@@ -1,12 +1,18 @@
 <template>
-  <div class="bg-english h-[550px] w-full">
+  <div class="bg-english h-[550px] w-full flex items-center justify-center">
+    <EnglishTile />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import EnglishTile from '@/components/atoms/EnglishTile.vue'
+
 export default defineComponent ({
   name: 'EnglishPage',
+  components: {
+    EnglishTile
+  }
 })
 
 </script>
