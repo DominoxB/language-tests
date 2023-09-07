0f87224780b3c3ef4d9dc6679670c706d7fb0011
Dominika <dominika-biedulska@tlen.pl>
2023-09-07 09:45:49 +0200

    feat: add border

--- src/components/atoms/TestSelectionDownItem.vue
+++ src/components/atoms/TestSelectionDownItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-blue-400 bg-white rounded-3xl py-12 px-16 border cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-blue-500">
+    <div class="border-blue-400 border-2 bg-white rounded-3xl py-12 px-16 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-blue-500">
       <img :src="flag" alt="flag" class="mx-auto rounded-lg w-12 mb-4"/>
       <div class="mb-2 uppercase font-semibold">{{ name }}</div>
       <span>{{ $t('solve') }}</span>
