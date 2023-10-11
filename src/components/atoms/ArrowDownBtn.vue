ea291015b258596a4177156596245548a3a8f7c6
Dominika <dominika-biedulska@tlen.pl>
2023-10-11 13:06:39 +0200

    fix: remove some unnecessary classes

--- src/components/atoms/ArrowDownBtn.vue
+++ src/components/atoms/ArrowDownBtn.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="rounded-full bg-blue-500 w-10 h-10 flex justify-center items-center transition ease-in-out delay-150 hover:scale-110 hover:bg-blue-400 duration-300">
+  <button class="rounded-full bg-blue-500 w-10 h-10 flex justify-center items-center transition hover:bg-blue-400">
     <img src="@/icons/down-arrow.png" alt="arrow-down">
   </button>
 </template>
