ef99b0c6f0b4dc3119af6db64a3f30755d05bac3
Dominika <dominika-biedulska@tlen.pl>
2023-10-17 12:55:20 +0200

    fix: set footer to every view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <top-navbar />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import TopNavbar from './components/organisms/TopNavbar.vue';
+import TopNavbar from './components/organisms/TopNavbar.vue'
+import Footer from '@/components/atoms/Footer.vue'
 export default defineComponent({
   name: 'App',
-  components: { TopNavbar }
+  components: { TopNavbar, Footer }
 })
 </script>
