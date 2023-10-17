0456d6ba9704f9ade4d1a54e053a22c9784eef39
Dominika <dominika-biedulska@tlen.pl>
2023-10-17 11:32:46 +0200

    feat: add btn start

---
+++ src/components/atoms/BtnStart.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-blue-200 py-2 px-6 rounded-md hover:bg-blue-300" @click="$emit('scroll')" >
+    Rozpocznij test
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "BtnArrowDown",
+  emits: ['scroll']
+})
+</script>
