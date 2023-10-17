08d60db64321337e7681396afcdbd6b875f03502
Dominika <dominika-biedulska@tlen.pl>
2023-10-17 10:30:06 +0200

    feat: create btn scroll to top

---
+++ src/components/atoms/BtnScrollToTop.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="rounded-xl bg-slate-300 w-10 h-10 flex justify-center items-center hover:bg-slate-400 z-10" @click="$emit('scroll')" >
+    <img src="@/icons/up-arrow.png" alt="arrow-up">
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "BtnArrowDown",
+  emits: ['scroll']
+})
+</script>
