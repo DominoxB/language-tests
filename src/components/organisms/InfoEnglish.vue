f9adc19e272d4617ead32b8ead4dbe527ffa0f0b
Dominika <dominika-biedulska@tlen.pl>
2023-10-18 11:39:04 +0200

    feat: add content and scroll after click btn

--- src/components/organisms/InfoEnglish.vue
+++ src/components/organisms/InfoEnglish.vue
@@ -5,19 +5,41 @@
     <p>{{ $t('info2') }}</p>
     <p class="py-6">{{ $t('info3') }}</p>
     <div class="flex justify-end">
-      <BtnStart class="mt-4" />
+      <BtnStart class="mt-4" @scroll="showTest" />
     </div>
   </div>
+  <IntroContent id="myScroll" v-if="test" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import BtnStart from '@/components/atoms/BtnStart.vue'
+import IntroContent from '@/components/organisms/IntroContent.vue';
 
 export default defineComponent({
   name: 'InfoEnglish',
   components: {
-    BtnStart
+    BtnStart,
+    IntroContent
+  },
+  setup() {
+    const test = ref(false)
+    const scrollToTest = () => {
+      const myScroll = document.getElementById("myScroll")
+      myScroll?.scrollIntoView({ behavior: "smooth", block: 'center' })
+    }
+
+    const showTest = () => {
+      test.value = true
+      setTimeout(() => {
+        scrollToTest()
+      }, 100);
+
+    }
+    return {
+      test,
+      showTest
+    }
   }
 })
 </script>
