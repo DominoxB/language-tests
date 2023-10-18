a680d057adcbf4c748cdfe2ebe80b2d78698b155
Dominika <dominika-biedulska@tlen.pl>
2023-10-18 12:56:52 +0200

    feat: create atom

---
+++ src/components/atoms/QuestionAndAnswers.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="font-open">
+    <span class="text-2xl">{{ question }}</span>
+    <div class="flex items-center space-x-6 text-lg border-2 border-blue-300 rounded-lg py-2 px-4 my-4">
+      <input type="radio" :value="answer" class="mr-1" /> 
+      {{ answer }}
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'QuestionAndAnswers',
+  props: {
+    question: {
+      type: String,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
