76fb24249733cf8bb934cfad20c6aba2f3a8eefe
Dominika <dominika-biedulska@tlen.pl>
2023-10-19 11:50:01 +0200

    fieat: add new properties to inputs

--- src/components/atoms/QuestionAndAnswers.vue
+++ src/components/atoms/QuestionAndAnswers.vue
@@ -4,20 +4,48 @@
       {{ number }} {{ question }}
     </div>
     <div class="flex items-center justify-center space-x-16 text-lg border-2 border-blue-300 rounded-lg py-4 px-4 my-4">
-      <input type="radio" :value="answerA" name="answer" />
-      {{ answerA }}
-      <input type="radio" :value="answerB" name="answer" />
-      {{ answerB }}
-      <input type="radio" :value="answerC" name="answer" />
-      {{ answerC }}
-      <input type="radio" :value="answerD" name="answer" />
-      {{ answerD }}
+      <div>
+        <input type="radio" 
+          :id="answerA" 
+          :value="answerA" 
+          :name="question" 
+          v-model="selected" />
+        {{ answerA }}
+        <label for="answerA" />
+      </div>
+      <div>
+        <input type="radio" 
+          :id="answerB" 
+          :value="answerB" 
+          :name="question" 
+          v-model="selected" />
+        {{ answerB }}
+        <label for="answerB" />
+      </div>
+      <div>
+        <input type="radio" 
+          :id="answerC" 
+          :value="answerC" 
+          :name="question" 
+          v-model="selected" />
+        {{ answerC }}
+        <label for="answerC" />
+      </div>
+      <div>
+         <input type="radio" 
+          :id="answerD" 
+          :value="answerD" 
+          :name="question" 
+          v-model="selected" />
+        {{ answerD }}
+        <label for="answerD" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
   name: 'QuestionAndAnswers',
   props: {
@@ -45,8 +73,12 @@ export default defineComponent({
       type: String,
       required: true
     }
-
-
+  },
+  setup() {
+    const selected = ref(null);
+    return {
+      selected
+    }
   }
 })
 </script>
