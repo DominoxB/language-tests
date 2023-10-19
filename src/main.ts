6733dbcceaf2cf4a8b8e85da2f0090ed0e7669f7
Dominika <dominika-biedulska@tlen.pl>
2023-10-19 10:07:33 +0200

    feat: add Pinia to project

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import App from './App.vue'
 import "./main.css"
 import i18n from './locales/index'
 import router from '@/router/index'
+import { createPinia } from 'pinia'
 
-createApp(App).use(i18n).use(router).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(i18n).use(router).use(pinia).mount('#app')
