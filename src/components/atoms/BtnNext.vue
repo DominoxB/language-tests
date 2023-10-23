2ff2d8301e7c235a98c56507a670a1ff677bc9cc
Dominika <dominika-biedulska@tlen.pl>
2023-10-23 13:05:44 +0200

    feat: add new btn

---
+++ src/components/atoms/BtnNext.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="bg-blue-200 py-2 px-6 rounded-md border border-blue-400 hover:bg-blue-300" @click="$emit('next')" >
+    {{ name }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: "BtnNext",
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['next']
+})
+</script>
