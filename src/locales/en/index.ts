bcf1c4e71479ca5362e87d1c6f047dbc62a8395b
Dominika <dominika-biedulska@tlen.pl>
2023-10-27 09:30:22 +0200

    fix: change desc

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,6 +1,6 @@
 const en = {
   question: 'Wondering how to check your language level?',
-  intro: "Each of us has tried to learn a foreign language at least once in our lives. The Lang4You application will help you check the effect of these trials. Thanks to specially developed tests, you will check your level and find out what parts of the material are to be repeated and which ones you are really excellent at 👌. After completing the test, you receive its result with answers and marking where you made a mistake. So, let's start?",
+  intro: "Each of us has learned a foreign language at least once in our lives. The Lang4You application will help you check your current language level. Thanks to specially developed tests, you will check your level and find out what parts of the material are to be repeated and which ones you are really excellent at 👌. After completing the test, you receive its result with answers and marking where you made a mistake. So, let's start?",
   freepik1:'Image by',
   freepik2: 'Freepik',
   test: 'Select a test',
--- src/locales/pl/index.ts
+++ src/locales/pl/index.ts
@@ -1,6 +1,6 @@
 const pl = {
   question: 'Zastanawiasz się, jak sprawdzić swój poziom językowy?',
-  intro: 'Każdy z nas przynajmniej raz w życiu próbował uczyć się języka obcego. Aplikacja Lang4You pomoże Ci sprawdzić, jaki efekt przyniosły te próby. Dzięki specjalnie opracowanym testom, sprawdzisz swój poziom i dowiesz się jakie partie materiału są do powtórki, a w jakich jesteś naprawdę doskonały 👌. Po wykonaniu testu otrzymujesz jego wynik wraz odpowiedziami i oznaczeniem, w którym miejscu popełniłeś błąd. To jak, startujemy?',
+  intro: 'Każdy z nas przynajmniej raz w życiu uczył się języka obcego. Aplikacja Lang4You pomoże Ci sprawdzić, jak wygląda Twój poziom językowy obecnie. Dzięki specjalnie opracowanym testom, sprawdzisz swój poziom i dowiesz się jakie partie materiału są do powtórki, a w jakich jesteś naprawdę doskonały 👌. Po wykonaniu testu otrzymujesz jego wynik wraz odpowiedziami i oznaczeniem, w którym miejscu popełniłeś błąd. To jak, startujemy?',
   freepik1: 'Zdjęcie ze strony',
   freepik2: 'Freepik',
   test: 'Wybierz test',
