2755d9417d4e5cf4cfa2d542bd5ff70d2dd73a1f
Dominika <dominika-biedulska@tlen.pl>
2023-11-02 10:41:44 +0100

    feat: use router-link into component

--- src/components/organisms/TopNavbar.vue
+++ src/components/organisms/TopNavbar.vue
@@ -3,7 +3,7 @@
     <NameAndLogo />
     <div class="flex space-x-10 mr-8">
       <TestSelectionNav />
-      <span>{{ $t('aboutUs') }}</span>
+      <router-link to="/AboutUsPage">{{ $t('aboutUs') }}</router-link>
       <div id="charger" class="text-white">
         <select v-model="$i18n.locale" class="cursor-pointer bg-blue-500">
           <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale"
