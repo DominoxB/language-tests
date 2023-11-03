30a619a4520377f42a2a0d0aaae6337c8253c591
Dominika <dominika-biedulska@tlen.pl>
2023-11-03 09:50:50 +0100

    fix: change text position and content

--- src/views/AboutUsPage.vue
+++ src/views/AboutUsPage.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-tutors w-full h-screen p-60 font-open text-2xl space-y-8 text-center">
+  <div class="bg-tutors w-full h-screen p-60 font-open text-2xl space-y-8 text-justify">
     <p class="text-4xl mb-16">O nas</p>
-    <p>Jesteśmy grupą przyjaciół pasjonujących się nauką języka obcego. Każdy z nas ma wykształcenie pedagogiczne oraz filologiczne, dlatego możemy powiedzieć, że jesteśmy secjalistami w tym temacie :) Oprócz tego lata doświadczeń jako nauczyciele/korepetytorzy dały nam mnóstwo inspiracji, pozowliły zaobserwować, jakie problemy pojawiają się podczas nauki, ale przede wszystkim, jak sobie z nimi radzić.</p>
-    <p>Testy przez nas przygotowane są sprawdzianem podstawowych umiejętności ucznia. Szeroki przekrój tematyczny i gramatyczny daje nam doskonały obraz tego, w jakim kierunku powinna toczyć się dalsza nauka. Jeśli chcesz nawiązać współpracę lub sprawdzić, co mamy Ci do zaoferowania skontaktuj się z nami po wykonaniu testu. Z nami języki obce przestaną być obce!</p>
+    <p>Jesteśmy grupą przyjaciół pasjonujących się nauką języka obcego. Każdy z nas ma wykształcenie pedagogiczne oraz filologiczne, dlatego możemy powiedzieć, że jesteśmy secjalistami w tym temacie :) Oprócz tego, lata doświadczeń jako nauczyciele/korepetytorzy, dały nam mnóstwo inspiracji, pozwoliły zaobserwować, jakie problemy pojawiają się podczas nauki, ale przede wszystkim, jak sobie z nimi radzić.</p>
+    <p>Testy przez nas przygotowane są sprawdzianem podstawowych umiejętności ucznia. Szeroki przekrój tematyczny i gramatyczny daje nam doskonały obraz tego, w jakim kierunku powinna toczyć się dalsza nauka. Jeśli chcesz nawiązać współpracę lub sprawdzić, co mamy Ci do zaoferowania skontaktuj się z nami po wykonaniu testu. Z nami języki przestaną być obce!</p>
   </div>
 </template>
 
