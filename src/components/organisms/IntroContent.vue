6fae30e0964c01eb62dc562928e6382c52371167
Dominika <dominika-biedulska@tlen.pl>
2023-11-03 13:15:49 +0100

    feat: rwd

--- src/components/organisms/IntroContent.vue
+++ src/components/organisms/IntroContent.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="w-full bg-test h-[550px] flex items-center justify-center font-open">
+  <div class="w-full bg-test md:h-[550px] flex items-center justify-center font-open">
     <transition name="text" appear enter-active-class="transition-all duration-1000 ease-in"
       enter-from-class="opacity-0 translate-x-[500px]">
-      <div class="mx-[500px] text-center text-white">
-        <p class="text-[45px] mb-4 font-semibold">
+      <div class="mx-20 md:mx-[500px] text-center text-white">
+        <p class="text-xl md:text-[45px] my-4 font-semibold">
           {{ $t('question') }}
         </p>
         <div class="bg-blue-500 w-64 h-1 mx-auto mb-8"></div>
-        <p class="text-xl mb-8">
+        <p class="md:text-xl mb-8">
           {{ $t('intro') }}
         </p>
         <BtnArrowDown class="mx-auto animate-bounce" @scroll="scrollDown" />
-        <p class="opacity-30 text-right text-sm">{{ $t('freepik1') }} <a
+        <p class="opacity-30 text-right text-xs md:text-sm">{{ $t('freepik1') }} <a
             href="https://www.freepik.com/free-photo/public-examination-preparation-concept_25077294.htm#query=answer%20sheet&position=1&from_view=search&track=ais#position=1&query=answer%20sheet">{{
               $t('freepik2') }}</a></p>
       </div>
