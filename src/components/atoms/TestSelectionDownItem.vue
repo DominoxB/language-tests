9314a61efa4b433103a883efc5ad2b1b13e18ad5
Dominika <dominika-biedulska@tlen.pl>
2023-11-03 13:20:47 +0100

    feat: add RWD

--- src/components/atoms/TestSelectionDownItem.vue
+++ src/components/atoms/TestSelectionDownItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="border-blue-400 border-2 bg-white rounded-3xl py-12 px-16 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-blue-500">
+    class="border-blue-400 border-2 bg-white rounded-3xl py-6 md:py-12 px-8 md:px-16 cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-blue-500">
     <img :src="flag" alt="flag" class="mx-auto rounded-lg w-12 mb-4" />
     <div class="mb-2 uppercase font-semibold">{{ name }}</div>
     <span>{{ $t('solve') }}</span>
