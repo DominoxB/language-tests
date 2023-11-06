6f24302c0e8adfa79ca62466867f18efe062c5b6
Dominika <dominika-biedulska@tlen.pl>
2023-11-06 13:29:33 +0100

    feat: add setting store on created

--- src/components/organisms/TestSheet.vue
+++ src/components/organisms/TestSheet.vue
@@ -1,35 +1,23 @@
 <template>
-  <div v-if="path === '/EnglishPage'">
-    <div id="myScroll" v-for="question in questionsEn.slice(start, end)" :key="question.id">
+  <div>
+    <div id="myScroll" v-for="question in questions.slice(start, end)" :key="question.id">
       <QuestionAndAnswers :id="question.id" :question="question.q" :answerA="question.a" :answerB="question.b"
         :answerC="question.c" :answerD="question.d" :correct="question.correct">
       </QuestionAndAnswers>
     </div>
     <div class="flex justify-center mx-96 space-x-24">
-      <BtnAction :name="$t('previous')" class="flex mb-6" @action="showPreviousQ" v-if="storeEn.currentPage > 1" />
-      <BtnAction :name="$t('next')" class="flex mb-6" @action="showNextQ" v-if="storeEn.currentPage < 3" />
-      <BtnAction :name="$t('checkResult')" class="flex mb-6" @action="showNextQ" v-if="storeEn.currentPage === 3" />
-    </div>
-  </div>
-  <div v-else>
-    <div id="myScroll" v-for="question in questionsRu.slice(start, end)" :key="question.id">
-      <QuestionAndAnswers :id="question.id" :question="question.q" :answerA="question.a" :answerB="question.b"
-        :answerC="question.c" :answerD="question.d" :correct="question.correct" />
-    </div>
-    <div class="flex justify-center mx-96 space-x-24">
-      <BtnAction :name="$t('previous')" class="flex mb-6" @action="showPreviousQ" v-if="storeRu.currentPage > 1" />
-      <BtnAction :name="$t('next')" class="flex mb-6" @action="showNextQ" v-if="storeRu.currentPage < 3" />
-      <router-link to="/ResultPage">
-        <BtnAction :name="$t('checkResult')" class="flex mb-6" v-if="storeRu.currentPage === 3" />
-      </router-link>
+      <BtnAction :name="$t('previous')" class="flex mb-6" @action="showPreviousQ" v-if="store.currentPage > 1" />
+      <BtnAction :name="$t('next')" class="flex mb-6" @action="showNextQ" v-if="store.currentPage < 3" />
+      <BtnAction :name="$t('checkResult')" class="flex mb-6" v-if="store.currentPage === 3" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, onUnmounted, ref } from 'vue'
 import { useQuestionEnglishStore } from '@/stores/testEnglish'
 import { useQuestionRussianStore } from '@/stores/testRussian'
+import { useUserAnswersStore } from '@/stores/userAnswers'
 import { useRoute } from 'vue-router'
 import QuestionAndAnswers from '../atoms/QuestionAndAnswers.vue'
 import BtnAction from '../atoms/BtnAction.vue'
@@ -40,15 +28,36 @@ export default defineComponent({
   setup() {
     const route = useRoute()
     const path = route.path
-
     const storeEn = useQuestionEnglishStore()
     const { questionsEn } = storeEn
 
     const storeRu = useQuestionRussianStore()
-    const { questionsRu, currentPage } = storeRu
+    const { questionsRu } = storeRu
+    
+    const storeAnswers = useUserAnswersStore()
 
     const start = ref(0)
     const end = ref(10)
+
+    const store = ref()
+    const questions = ref()
+    const currentPage = ref(1)
+
+    const setStore = () => {
+      if (path === '/EnglishPage') {
+        store.value = storeEn
+        questions.value = questionsEn
+      } else {
+        store.value = storeRu
+        questions.value = questionsRu
+      }
+    }
+
+    setStore()
+
+    onUnmounted(() => {
+      storeAnswers.$reset()
+    })
 
     const scrollToBeginning = () => {
       const myScroll = document.getElementById("myScroll")
@@ -56,8 +65,7 @@ export default defineComponent({
     }
 
     const showNextQ = () => {
-      storeEn.increment()
-      storeRu.increment()
+      store.value.increment()
       start.value += 10
       end.value += 10
       setTimeout(() => {
@@ -66,8 +74,7 @@ export default defineComponent({
     }
 
     const showPreviousQ = () => {
-      storeEn.decrement()
-      storeRu.decrement()
+      store.value.decrement()
       start.value -= 10
       end.value -= 10
       setTimeout(() => {
@@ -76,16 +83,14 @@ export default defineComponent({
     }
 
     return {
-      questionsEn,
-      questionsRu,
+      store,
+      questions,
       currentPage,
       path,
       start,
       end,
       showNextQ,
       showPreviousQ,
-      storeRu,
-      storeEn
     }
   }
 })
