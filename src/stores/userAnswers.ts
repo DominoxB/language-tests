2172f85ee72110a6048899f4bbc8fb6b1dd8c27b
Dominika <dominika-biedulska@tlen.pl>
2023-11-09 17:58:07 +0100

    feat: update store

--- src/stores/userAnswers.ts
+++ src/stores/userAnswers.ts
@@ -2,15 +2,16 @@ import { defineStore } from "pinia";
 
 // API simulation (mocked data)
 export const useUserAnswersStore = defineStore("UserAnswers", {
-  state: () => ({
-    answers: {} as string,
-    correctAnswers: {} as string,
-    counter: 0
-  }),
+  state: () => {
+    return {
+      answers: {} as string,
+      correctAnswers: {} as string,
+      counter: 0
+    }
+  },
   actions: {
     addPoint() {
       this.counter++
-      console.log(this.counter, 'counter')
     },
   }
 })
