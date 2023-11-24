ab50c6652ddb389a5255903ee680bbb4609b4e37
Dominika <dominika-biedulska@tlen.pl>
2023-11-24 13:49:37 +0100

    feat: create separate component for lang charger

---
+++ src/components/atoms/LanguageCharger.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="charger" class="text-white">
+    <select v-model="$i18n.locale" class="cursor-pointer bg-blue-500">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale"
+        class="bg-blue-500 hover:bg-blue-400">
+        {{ locale }}
+      </option>
+    </select>
+  </div>
+</template>
