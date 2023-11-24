468775355c71ae394d0c522a68c946609b951c03
Dominika <dominika-biedulska@tlen.pl>
2023-11-24 13:55:36 +0100

    fix: get bigger name

--- src/components/molecules/NameAndLogo.vue
+++ src/components/molecules/NameAndLogo.vue
@@ -2,7 +2,7 @@
   <router-link to="/">
     <div class="flex items-center">
       <img src="@/icons/logo.png" alt="logo" class="w-8 md:w-12 mr-2 md:mr-4" />
-      <span class="font-courgette text-xl md:text-2xl text-white">Lang4You</span>
+      <span class="font-courgette text-2xl text-white">Lang4You</span>
     </div>
   </router-link>
 </template>
