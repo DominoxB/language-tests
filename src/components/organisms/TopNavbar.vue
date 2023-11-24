0178b03347b44bf2fb06fe6997c392214d4c6b11
Dominika <dominika-biedulska@tlen.pl>
2023-11-24 13:54:02 +0100

    fix: rebuild  mobile top navbar

--- src/components/organisms/TopNavbar.vue
+++ src/components/organisms/TopNavbar.vue
@@ -1,22 +1,19 @@
 <template>
   <div
-    class="bg-blue-500 px-2 md:px-8 py-4 flex justify-between items-center text-white font-open md:text-lg top-0 sticky z-20">
+    class="hidden md:flex bg-blue-500 px-2 md:px-8 py-4 justify-between items-center text-white font-open md:text-lg top-0 sticky z-20">
     <NameAndLogo />
-    <div class="hidden md:flex">
+    <div class="flex">
       <div class="flex space-x-1 md:space-x-10 md:mr-8">
         <TestSelectionNav />
         <router-link to="/AboutUsPage">{{ $t('aboutUs') }}</router-link>
-        <div id="charger" class="text-white">
-          <select v-model="$i18n.locale" class="cursor-pointer bg-blue-500">
-            <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale"
-              class="bg-blue-500 hover:bg-blue-400">
-              {{ locale }}
-            </option>
-          </select>
-        </div>
+        <LanguageCharger />
       </div>
     </div>
-    <NavbarHamburger class="flex md:hidden" />
+  </div>
+  <div class="flex md:hidden bg-blue-500 px-2 py-4 justify-between items-center text-white font-open top-0 sticky z-20">
+    <NavbarHamburger />
+    <NameAndLogo />
+    <LanguageCharger />
   </div>
 </template>
 
@@ -25,12 +22,14 @@ import { defineComponent } from 'vue'
 import NameAndLogo from '../molecules/NameAndLogo.vue'
 import TestSelectionNav from '../organisms/TestSelectionNav.vue'
 import NavbarHamburger from '../atoms/NavbarHamburger.vue'
+import LanguageCharger from '../atoms/LanguageCharger.vue'
 export default defineComponent({
   name: 'TopNavbar',
   components: {
     NameAndLogo,
     TestSelectionNav,
-    NavbarHamburger
+    NavbarHamburger,
+    LanguageCharger
   },
 })
 </script>
