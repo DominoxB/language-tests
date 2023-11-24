8b23f4a8cf97ba6a213b9b2d06bbf27c5e3e4cc1
Dominika <dominika-biedulska@tlen.pl>
2023-11-24 10:45:23 +0100

    feat: rebuild mobile menu

--- src/components/atoms/NavbarHamburger.vue
+++ src/components/atoms/NavbarHamburger.vue
@@ -3,46 +3,32 @@
     <button @click="clickHamburger" class="relative group">
       <div class="flex overflow-hidden items-center justify-center z-60">
         <div
-          class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all origin-center overflow-hidden"
-        >
-          <div
-            :class="{ 'translate-x-10': show }"
-            class="bg-white h-[2px] w-7 origin-left"
-          ></div>
-          <div
-            :class="{ 'translate-x-10': show }"
-            class="bg-white h-[2px] w-7 origin-left"
-          ></div>
-          <div
-            :class="{ 'translate-x-10': show }"
-            class="bg-white h-[3px] w-7 origin-left"
-          ></div>
-          <div
-            class="absolute items-center justify-between transform transition-all top-2.5 flex"
-          >
-            <div
-              :class="{ 'rotate-45': show }"
-              class="absolute bg-white h-[2px] w-5 transform transition-all duration-200 delay-100"
-            ></div>
-            <div
-              :class="{ '-rotate-45': show }"
-              class="absolute bg-white h-[2px] w-5 transform transition-all duration-200 delay-100"
-            ></div>
+          class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all origin-center overflow-hidden">
+          <div :class="{ 'translate-x-10': show }" class="bg-white h-[2px] w-7 origin-left"></div>
+          <div :class="{ 'translate-x-10': show }" class="bg-white h-[2px] w-7 origin-left"></div>
+          <div :class="{ 'translate-x-10': show }" class="bg-white h-[3px] w-7 origin-left"></div>
+          <div class="absolute items-center justify-between transform transition-all top-2.5 flex">
+            <div :class="{ 'rotate-45': show }"
+              class="absolute bg-white h-[2px] w-5 transform transition-all duration-200 delay-100"></div>
+            <div :class="{ '-rotate-45': show }"
+              class="absolute bg-white h-[2px] w-5 transform transition-all duration-200 delay-100"></div>
           </div>
         </div>
       </div>
     </button>
-    <div
-      :class="{ '-translate-x-full': !show }"
-      class="fixed h-[104px] w-full block text-lg text-white text-center py-2 bg-blue-600 top-16 bottom-0 left-0 transition duration-700"
-    >
-      <TestSelectionNav
-        class="hover:bg-blue-500 py-2 cursor-pointer text-center"
-      />
-      <p
-        @click="clickLink('/AboutUsPage')"
-        class="hover:bg-blue-500 py-2 cursor-pointer"
-      >
+    <div :class="{ '-translate-x-full': !show }"
+      class="fixed h-[148px] w-full block text-lg text-white text-center border py-2 bg-blue-600 top-16 bottom-0 left-0 transition duration-300">
+      <div class="py-2 flex items-center justify-center hover:bg-blue-500 cursor-pointer"
+        @click="clickLink('/EnglishPage')">
+        <img src="https://img.icons8.com/offices/30/great-britain.png" alt="flag" class="w-5 mr-2" />
+        <span>{{ $t('english') }}</span>
+      </div>
+      <div class="py-2 flex items-center justify-center hover:bg-blue-500 cursor-pointer"
+        @click="clickLink('/RussianPage')">
+        <img src="https://img.icons8.com/offices/30/russian-federation.png" alt="flag" class="w-5 mr-2" />
+        <span>{{ $t('russian') }}</span>
+      </div>
+      <p class="hover:bg-blue-500 py-2 cursor-pointer" @click="clickLink('/AboutUsPage')">
         {{ ('O nas') }}
       </p>
     </div>
@@ -50,13 +36,11 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue"
-import { useRouter } from "vue-router"
-import TestSelectionNav from "../organisms/TestSelectionNav.vue"
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: "NavbarHamburger",
-  components: { TestSelectionNav},
   setup(props, context) {
     const router = useRouter()
     const show = ref(false)
@@ -70,7 +54,7 @@ export default defineComponent({
       show.value = false;
       router.push({ path: url })
     }
-    
+
     return {
       clickHamburger,
       clickLink,
--- src/components/molecules/TestItems.vue
+++ src/components/molecules/TestItems.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-2 border absolute hidden md:block">
+  <div class="mt-2 border absolute">
     <router-link to="/EnglishPage">
       <div class="p-2 w-32 flex items-center hover:bg-slate-600 text-white bg-slate-800 cursor-pointer">
         <img src="https://img.icons8.com/offices/30/great-britain.png" alt="flag" class="w-5 mr-2" />
@@ -13,19 +13,4 @@
       </div>
     </router-link>
   </div>
-  <div class="mt-2 border md:hidden">
-    <router-link to="/EnglishPage">
-      <div class="p-2 flex items-center justify-center hover:bg-blue-500  text-center bg-blue-600 cursor-pointer">
-        <img src="https://img.icons8.com/offices/30/great-britain.png" alt="flag" class="w-5 mr-2" />
-        <span>{{ $t('english') }}</span>
-      </div>
-    </router-link>
-    <router-link to="/RussianPage">
-      <div class="p-2 flex items-center justify-center hover:bg-blue-500 text-white text-center bg-blue-600 cursor-pointer">
-        <img src="https://img.icons8.com/offices/30/russian-federation.png" alt="flag" class="w-5 mr-2" />
-        <span>{{ $t('russian') }}</span>
-      </div>
-    </router-link>
-  </div>
-
 </template>
