12288a96d1b67d24d780081a5eecabdf0381416f
Dominika <dominika-biedulska@tlen.pl>
2023-12-04 16:04:29 +0100

    fix: change home path

--- src/components/molecules/NameAndLogo.vue
+++ src/components/molecules/NameAndLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to="/">
+  <router-link to="/language-tests">
     <div class="flex items-center">
       <img src="@/icons/logo.png" alt="logo" class="w-8 md:w-12 mr-2 md:mr-4" />
       <span class="font-courgette text-2xl text-white">Lang4You</span>
