962003201da9770d260fe95862a87440bf345d5b
Dominika <dominika-biedulska@tlen.pl>
2023-12-04 15:24:45 +0100

    feat: add dist

--- src/components/atoms/IntroResult.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="text-center bg-test bg-cover py-24  md:py-36 px-4 md:px-[120px] font-open text-white">
-    <p class="text-2xl md:text-3xl font-bold mb-4">{{ $t('introResult') }}</p>
-    <div class="space-y-1 text-lg">
-      <p>{{ $t('introResult1') }}</p>
-      <p>{{ $t('introResult2') }}</p>
-    </div>
-  </div>
-</template>
